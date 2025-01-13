bbefe9ca8f50b9c8d1da215254ff42b333b800ee
Asparrow13 <145340503+Asparrow13@users.noreply.github.com>
2025-01-13 19:24:48 +0100

    MLMovie & MCController

--- src/main/java/dk/easv/moviecollectionproject/GUI/Controller/MCController.java
+++ src/main/java/dk/easv/moviecollectionproject/GUI/Controller/MCController.java
@@ -1,17 +1,37 @@
 package dk.easv.moviecollectionproject.GUI.Controller;
 
+import dk.easv.moviecollectionproject.BE.Movie;
+import dk.easv.moviecollectionproject.BLL.BLMovie;
+import dk.easv.moviecollectionproject.GUI.Model.MLMovie;
 import dk.easv.moviecollectionproject.BE.Category;
 import dk.easv.moviecollectionproject.BLL.BLCategory;
 import dk.easv.moviecollectionproject.GUI.Model.MLCategory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import javafx.scene.control.Label;
+
 public class MCController {
+
+    @FXML
+    private TableView<MLMovie> movieTableView;
+
+    @FXML
+    private TableColumn<MLMovie, String> nameColumn;
+
+    @FXML
+    private TableColumn<MLMovie, Float> ratingColumn;
+
+    @FXML
+    private TableColumn<MLMovie, Integer> categoryColumn;
+
+    @FXML
+    private TableColumn<MLMovie, String> lastviewColumn;
+
 
     @FXML
     private TableView<MLCategory> categoryTableView;
@@ -19,11 +39,26 @@ public class MCController {
     @FXML
     private TableColumn<MLCategory, String> categoryNameColumn;
 
+    private final BLMovie blMovie = new BLMovie();
+    private final ObservableList<MLMovie> movies = FXCollections.observableArrayList();
+
     private final BLCategory blCategory = new BLCategory();
     private final ObservableList<MLCategory> categories = FXCollections.observableArrayList();
 
+
+    // Initializer
     @FXML
     public void initialize() {
+        // Set up columns to use the respective properties
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+        lastviewColumn.setCellValueFactory(new PropertyValueFactory<>("lastView"));
+
+        // Load movies into the TableView
+        loadMovies();
+
+
         // Set up the table column to use the name property
         categoryNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
@@ -31,6 +66,16 @@ public class MCController {
         loadCategories();
     }
 
+    // Movie Management
+    private void loadMovies() {
+        movies.clear();
+        for (Movie movie : blMovie.getAllMovie()) {
+            movies.add(new MLMovie(movie));
+        }
+        movieTableView.setItems(movies);
+    }
+
+    // Category Management
     private void loadCategories() {
         categories.clear();
         for (Category category : blCategory.getAllCategories()) {
--- src/main/java/dk/easv/moviecollectionproject/GUI/Model/MLMovie.java
+++ src/main/java/dk/easv/moviecollectionproject/GUI/Model/MLMovie.java
@@ -1,4 +1,95 @@
 package dk.easv.moviecollectionproject.GUI.Model;
 
+import dk.easv.moviecollectionproject.BE.Movie;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.sql.Date;
+
 public class MLMovie {
+    private final SimpleIntegerProperty id;
+    private final SimpleStringProperty name;
+    private final SimpleFloatProperty rating;
+    private final SimpleIntegerProperty category;
+    private final SimpleStringProperty filePath;
+    private final SimpleObjectProperty<Date> lastView;
+
+    public MLMovie(Movie movie) {
+        this.id = new SimpleIntegerProperty(movie.getId());
+        this.name = new SimpleStringProperty(movie.getName());
+        this.rating = new SimpleFloatProperty(movie.getRating());
+        this.category = new SimpleIntegerProperty(movie.getCategory());
+        this.filePath = new SimpleStringProperty(movie.getFilePath());
+        this.lastView = new SimpleObjectProperty<>(movie.getLastView());
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public float getRating() {
+        return rating.get();
+    }
+
+    public SimpleFloatProperty ratingProperty() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating.set(rating);
+    }
+
+    public int getCategory() {
+        return category.get();
+    }
+
+    public SimpleIntegerProperty categoryProperty() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category.set(category);
+    }
+
+    public String getFilePath() {
+        return filePath.get();
+    }
+
+    public SimpleStringProperty filePathProperty() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath.set(filePath);
+    }
+
+    public Date getLastView() {
+        return lastView.get();
+    }
+
+    public SimpleObjectProperty<Date> lastViewProperty() {
+        return lastView;
+    }
+
+    public void setLastView(Date lastView) {
+        this.lastView.set(lastView);
+    }
 }
